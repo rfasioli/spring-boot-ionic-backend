58397ada3b229d7ee8a314d5d711a1a0954fadf4
Rodrigo Fasioli <rodrigofasioli@brq.com>
2018-05-19 21:11:32 -0300

    Instance data for Categoria

--- src/main/java/br/com/rfasioli/cursomc/CursomcApplication.java
+++ src/main/java/br/com/rfasioli/cursomc/CursomcApplication.java
@@ -1,12 +1,36 @@
 package br.com.rfasioli.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.rfasioli.cursomc.domain.Categoria;
+import br.com.rfasioli.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		this.carregaCategoria();		
+	}
+	
+	private void carregaCategoria() {
+		
+		Categoria cat1 = new Categoria(null, "Informática");		
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 }
