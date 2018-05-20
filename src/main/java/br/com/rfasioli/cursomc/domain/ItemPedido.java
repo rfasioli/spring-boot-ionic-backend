d496235c951e15145a77ab72e6cbe01af63f866c
Rodrigo Fasioli <rodrigofasioli@brq.com>
2018-05-20 13:08:09 -0300

    Pedidos endpoint available

--- src/main/java/br/com/rfasioli/cursomc/domain/ItemPedido.java
+++ src/main/java/br/com/rfasioli/cursomc/domain/ItemPedido.java
@@ -5,10 +5,13 @@ import java.io.Serializable;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class ItemPedido implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPk id = new ItemPedidoPk();
 	
@@ -38,6 +41,7 @@ public class ItemPedido implements Serializable {
 		this.id = id;
 	}
 	
+	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
