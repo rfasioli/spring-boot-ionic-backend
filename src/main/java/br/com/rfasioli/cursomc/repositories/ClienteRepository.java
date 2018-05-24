b275d6b98845be0c1bb757dc354319d4ea3fd22e
Rodrigo Fasioli <rodrigofasioli@brq.com>
2018-05-24 20:34:27 -0300

    Custom validation for email

--- src/main/java/br/com/rfasioli/cursomc/repositories/ClienteRepository.java
+++ src/main/java/br/com/rfasioli/cursomc/repositories/ClienteRepository.java
@@ -2,10 +2,17 @@ package br.com.rfasioli.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.rfasioli.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
+	
+	@Transactional(readOnly=true)
+	long countByEmail(String email);
+	
 }
--- src/main/java/br/com/rfasioli/cursomc/services/validation/ClientInsertValidator.java
+++ src/main/java/br/com/rfasioli/cursomc/services/validation/ClientInsertValidator.java
@@ -6,13 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import br.com.rfasioli.cursomc.domain.enums.TipoCliente;
 import br.com.rfasioli.cursomc.dto.ClienteNewDTO;
+import br.com.rfasioli.cursomc.repositories.ClienteRepository;
 import br.com.rfasioli.cursomc.resources.exception.FieldMessage;
 import br.com.rfasioli.cursomc.services.validation.utils.DocumentUtilBR;
 
 public class ClientInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO>{
 
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 		
@@ -30,6 +36,10 @@ public class ClientInsertValidator implements ConstraintValidator<ClienteInsert,
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido"));
 		}
 		
+		if (clienteRepository.countByEmail(objDto.getEmail()) > 0) {
+			list.add(new FieldMessage("Email", "Email já cadastrado."));			
+		}
+
 		for (FieldMessage fieldMessage : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(fieldMessage.getMessage()).
